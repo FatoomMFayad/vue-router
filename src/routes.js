8ec7a3dae23493dfdc15c4e79fe18d3347f87179
FatoomMFayad <fatoomfayyad@gmail.com>
2020-09-21 13:00:16 +0300

    Catch all routes wildcards

--- src/routes.js
+++ src/routes.js
@@ -19,6 +19,7 @@ export const routes = [
         { path: ':id', component: UserDetail },
         { path: ':id/edit', component: UserEdit, name: 'userEdit'}
     ] },
-    { path:'/redirect-me', redirect: '/user' }
+    { path:'/redirect-me', redirect: '/user' },
+    { path: '*', redirect: '/'}
 
 ];
